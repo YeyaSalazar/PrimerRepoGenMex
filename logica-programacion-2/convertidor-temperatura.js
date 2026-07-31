31295f0cd59afd19e2b2e9267b06af72d21e6314
Iran Gutierrez <76507028+YeyaSalazar@users.noreply.github.com>
2026-07-31 13:51:35 -0600

    Add files via upload

---
+++ logica-programacion-2/convertidor-temperatura.js
@@ -0,0 +1,15 @@
+
+let celciusInput = prompt("Por favor, ingresa la temperatura en C° con decimales:");
+
+
+
+function transforToFahr(){
+    // La fórmula para convertir grados celsius en Fahrenheit es: °F = (°C × 9/5) + 32. 
+    // Multiplica los grados Celsius por 9/5 y luego suma 32.
+    return F = (celciusInput * (9/5) + 32);
+}
+
+function transToKelvin(){
+    //La fórmula para convertir Celsius (°C) en Kelvin (K) es la siguiente: K = °C + 273,15. 
+    return K = celciusInput + 273.15;
+}
